c71da7d975f3296f2a27a347af50b8cbc358e6e3
roxasz <roxasz@roxasz.roxasz>
2024-10-16 19:34:42 +0700

    backendApi

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/lp-data", (req, res) => {
+  res.json({
+    name: "John Doe",
+    age: 25,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
