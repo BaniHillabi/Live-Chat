34a76443aeeb6799e4e16637de9a269d3aa748ba
Naikod <gamergmsns@gmail.com>
2024-10-16 19:42:42 +0700

    Adding Data of Rest API

--- Backend/server.js
+++ Backend/server.js
@@ -4,8 +4,47 @@ const app = express();
 
 app.get("/lp-data", (req, res) => {
   res.json({
-    name: "John Doe",
-    age: 25,
+    data: {
+        offers: [
+            {
+                name: "Pro",
+                price: "$29.99 / Month",
+                feature: [
+                    {name: "Website & Mobile Optimized", unlock: true},
+                    {name: "Life-time Free Usage", unlock: true},
+                    {name: "Software Update", unlock: true},
+                    {name: "6 Month Support", unlock: true},
+                    {name: "No Installation Charges", unlock: false},
+                    {name: "Inbuilt Malware Scanner", unlock: false}
+                ]
+            },
+            {
+                name: "Best",
+                price: "$79.99 / Month",
+                feature: [
+                    {name: "Website & Mobile Optimized", unlock: true},
+                    {name: "Life-time Free Usage", unlock: true},
+                    {name: "Software Update", unlock: true},
+                    {name: "6 Month Support", unlock: true},
+                    {name: "No Installation Charges", unlock: true},
+                    {name: "Inbuilt Malware Scanner", unlock: false}
+                ]
+            },
+            {
+                name: "Enterprise",
+                price: "$149.99 / Month",
+                feature: [
+                    {name: "Website & Mobile Optimized", unlock: true},
+                    {name: "Life-time Free Usage", unlock: true},
+                    {name: "Software Update", unlock: true},
+                    {name: "6 Month Support", unlock: true},
+                    {name: "No Installation Charges", unlock: true},
+                    {name: "Inbuilt Malware Scanner", unlock: true}
+                ]
+            },
+
+        ]
+    }
   });
 });
 
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 export default function Page() {
   return (
     <div>
-      <Button className="bg-red-600" >Click me</Button>
+      <Button variant="link">Click me</Button>
     </div>
   )
 }
